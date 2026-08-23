86e97503cb360d13db414aa490056efb521bb932
AliCetin06 <aucetinn@gmail.com>
2026-08-23 22:17:23 +0300

    update driver commit

--- utils/driver_utils.py
+++ utils/driver_utils.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from typing import Optional
+import os
 
 
 class DriverUtils:
@@ -9,12 +11,23 @@ class DriverUtils:
     def create_driver(cls) -> None:
         """
         Chrome WebDriver oturumunu Selenium 4 standartlarına göre otomatik başlatır.
+        CI ortamında (GitHub Actions) otomatik olarak headless modda çalışır.
         """
-        # Service veya manuel dosya yolu tanımı kaldırıldı.
-        # Selenium 4 tarayıcınıza uygun driver'ı otomatik olarak yönetir.
-        cls._driver = webdriver.Chrome()
+        options = Options()
+
+        is_ci = os.environ.get("CI") == "true" or os.environ.get("GITHUB_ACTIONS") == "true"
+
+        if is_ci:
+            options.add_argument("--headless=new")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
+            options.add_argument("--window-size=1920,1080")
+
+        cls._driver = webdriver.Chrome(options=options)
         cls._driver.implicitly_wait(60)
-        cls._driver.maximize_window()
+
+        if not is_ci:
+            cls._driver.maximize_window()
 
     @classmethod
     def get_driver(cls) -> webdriver.Chrome:
